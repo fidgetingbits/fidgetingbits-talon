5a9e8187e91c43452467b03e3e7102759c79e94e
Aaron Adams <aaron.adams@nccgroup.com>
2021-07-28 10:53:49 +0800

    action for printing output from externally run commands

--- code/exec.py
+++ code/exec.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 
+from talon import actions
 from talon import Module
 
 mod = Module()
@@ -15,3 +16,9 @@ class Actions:
     def system_command_nb(cmd: str):
         """execute a command on the system without blocking"""
         subprocess.Popen(cmd, shell=True)
+
+    def system_command_output(cmd: str):
+        """execute a command on the system and return it's output"""
+
+        output = subprocess.check_output(cmd.split(" "), shell=True)
+        actions.insert(output.decode("utf-8"))
