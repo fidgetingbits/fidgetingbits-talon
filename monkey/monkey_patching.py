d4bd45a4977e1b0269542df22e5906fa6581e7b3
Aaron Adams <aaron.adams@nccgroup.com>
2021-11-16 10:43:48 +0800

    harden monkey patching to avoid people reporting problems to aegis

--- monkey/monkey_patching.py
+++ monkey/monkey_patching.py
@@ -1,7 +1,9 @@
 import os
+import pathlib
 import subprocess
 
 from talon import app, imgui, ui, scripting
+
 
 def monkey_notify(body="", title="", subtitle="", *kwargs):
     subprocess.Popen(
@@ -10,21 +12,60 @@ def monkey_notify(body="", title="", subtitle="", *kwargs):
 
 
 def monkey_focus(self, *kwargs):
-    # subprocess.Popen(["i3-msg", f"'[class=\"(?]){self.name}\"] focus'"])
     if self.active_window.id == -1:
         os.system("i3-msg '[class=\"(?)%s\"] focus'" % self.name)
     else:
         os.system("i3-msg '[id=\"%s\"] focus'" % self.active_window.id)
 
-# patch this to use dunst on i3wm
-app.notify = monkey_notify
 
-# patch this to use i3-msg on i3wm
-ui.App.focus = monkey_focus
+def install_monkey_show():
+    """ On i3wm the pop up windows often minimize to unviewable size
+
+    Also no way to actually resize them. show() auto refreshes,
+    so is more cpu intensive, but does not have this problem
+    """
+
+    print("Installing fidget imgui.GUI freeze override monkey patch")
+    imgui.GUI.freeze = imgui.GUI.show
 
 
+def install_monkey_focus():
+    """Install a monkey patch to use i3 to focus windows."""
 
-# on i3wm the pop up windows often minimize to and not inviewable size, with no
-# way to actually resize them. show() auto refreshes, so is more cpu intensive,
-# but does not have this problem
-imgui.GUI.freeze = imgui.GUI.show
+    out = None
+    try:
+        out = subprocess.check_output("which i3-msg", shell=True)
+    except subprocess.CalledProcessError:
+        print("WARNING: i3-msg not found. Skipping monkey patch")
+    if not out:
+        print("Installing fidget i3-msg focus monkey patch")
+        ui.App.focus = monkey_focus
+
+
+def install_monkey_notify():
+    """Install a monkey patch to use dunst instead of qt notifications.
+
+    This is mostly useful for things like i3, or if you just generally think
+    the notifications are too hard to notice.
+    """
+
+    out = None
+    try:
+        out = subprocess.check_output("which notify-send.sh", shell=True)
+    except subprocess.CalledProcessError:
+        print("WARNING: notify-send.sh not found. Skipping monkey patch")
+    if not out:
+        print("Installing fidget notify-send.sh notify monkey patch")
+        app.notify = monkey_notify
+
+
+def install_monkey_patches():
+    """Installs a number of monkey patches if the associated tools are found"""
+
+    print("Installing fidget monkey patches")
+    install_monkey_notify()
+    install_monkey_focus()
+    install_monkey_show()
+
+
+install_monkey_patches()
