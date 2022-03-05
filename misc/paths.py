bf5afcf6e71313d9509bada162631443eb22be06
Aaron Adams <aaron.adams@nccgroup.com>
2022-03-05 14:41:37 +0800

    add some more standard paths

--- misc/paths.py
+++ misc/paths.py
@@ -10,8 +10,10 @@ mod.list("folder_paths_public", desc="Common paths")
 mod.list("folder_paths_private", desc="Common private paths")
 ctx = Context()
 
-TALON_REPO="fidget"
-arch_linux_paths = { "packman hooks": "/etc/pacman.d/hooks/",}
+TALON_REPO = "fidget"
+arch_linux_paths = {
+    "packman hooks": "/etc/pacman.d/hooks/",
+}
 # paths that will work with pivot command
 linux_folder_paths = {
     "user services": "~/.config/systemd/user/",
@@ -49,7 +51,10 @@ linux_folder_paths = {
     "back": "../",
     "tunnel": "~/.ssh/",
     "S S H": "~/.ssh/",
-
+    "raffi": "~/.config/rofi",
+    "network manager": "/etc/NetworkManager/",
+    "network manager dispatcher": "/etc/NetworkManager/dispatcher.d",
+    "network manager config": "/etc/NetworkManager/conf.d",
     # ubuntu-esque stuff
     "lib linux": "/lib/x86_64-linux-gnu/",
     "lib sixty four": "/lib64/",
@@ -70,19 +75,21 @@ linux_file_paths = {
     "f stab": "/etc/fstab",
     "make init config": "/etc/mkinitcpio.conf",
     "boot config": "/boot/grub/grub.cfg",
-    "journal config": "/etc/systemd/journald.conf", 
-    "tunnel config": "~/.ssh/config", 
-    "shell config": "~/.zshrc", 
-    "git config": ".git/config", 
-    "poly bar": "~/.config/polybar/config", 
-    "eye three": "~/.i3/config", 
-    "vim config": "~/.vimrc", 
+    "journal config": "/etc/systemd/journald.conf",
+    "tunnel config": "~/.ssh/config",
+    "shell config": "~/.zshrc",
+    "git config": ".git/config",
+    "poly bar": "~/.config/polybar/config",
+    "eye three": "~/.i3/config",
+    "vim config": "~/.vimrc",
     "c snippets": "~/.vim/plugged/vim-snippets/UltiSnips/c.snippets",
     "mark down snippets": "~/.vim/plugged/vim-snippets/UltiSnips/markdown.snippets",
     "python snippets": "~/.vim/plugged/vim-snippets/UltiSnips/python.snippets",
     "bash snippets": "~/.vim/plugged/vim-snippets/UltiSnips/bash.snippets",
-    "bash snippets": "~/.config/kitty/kitty.conf",
+    "kitty config": "~/.config/kitty/kitty.conf",
     "grub defaults": "/etc/default/grub",
+    "raffi config": "~/.config/rofi/rofi.rasi",
+    "raffi theme": "~/.config/rofi/colors.rasi",
 }
 
 # this is used for specific commands like pivot
@@ -93,10 +100,7 @@ ctx.lists["user.folder_paths_private"] = {}
 
 # this is used for any path based commands that don't care of about files or
 # folder difference
-ctx.lists["user.paths_public"] = {
-    **linux_folder_paths,
-    **linux_file_paths
-}
+ctx.lists["user.paths_public"] = {**linux_folder_paths, **linux_file_paths}
 
 # Arch Linux
 
@@ -113,46 +117,51 @@ windows_paths = {
     "programs": "%PROGRAMFILES%",
 }
 
+
 @mod.capture(rule="{user.folder_paths_public}")
-def folder_paths_public(m)-> str:
+def folder_paths_public(m) -> str:
     "One path representing a public folder"
     return m.folder_paths_public
 
+
 @mod.capture(rule="{user.folder_paths_private}")
-def folder_paths_private(m)-> str:
+def folder_paths_private(m) -> str:
     "One path representing a public folder"
     return m.folder_paths_private
 
 
 @mod.capture(rule="{user.paths_public}")
-def paths_public(m)-> str:
+def paths_public(m) -> str:
     "One public path representing a file or folder"
     return m.paths_public
 
 
 @mod.capture(rule="{user.paths_private}")
-def paths_private(m)-> str:
+def paths_private(m) -> str:
     "One private path representing a file or folder"
     return m.paths_private
 
+
 @mod.capture(rule="<user.folder_paths_public>|<user.folder_paths_private>")
-def folder_paths(m)-> str:
+def folder_paths(m) -> str:
     "One public or private path that represents a folder"
     return m
 
+
 @mod.capture(rule="<user.paths_public>|<user.paths_private>")
-def paths(m)-> str:
+def paths(m) -> str:
     "One public or private path that represents a file or folder"
     return m
 
+
 @mod.action_class
 class Actions:
-    def path_traverse(num:int) -> str:
+    def path_traverse(num: int) -> str:
         """creates a string num path traversal sequences"""
-        return "../"*num
+        return "../" * num
 
     # XXX - this should be an overridable type of method so that we can
     # have language specific escaping, for multiple types of scenarios
-    def escape_path(path:str):
+    def escape_path(path: str):
         """Escape separators in a path string"""
         return path.replace("/", "\\/")
