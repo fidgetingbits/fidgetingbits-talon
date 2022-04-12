103e35f2de721f4c1e11771c4292c03b2d3c3417
Aaron Adams <aaron.adams@nccgroup.com>
2022-04-12 11:00:21 +0800

    more files

---
+++ apps/platforms/linux/nm_applet/nm_applet.py
@@ -0,0 +1,4 @@
+# basically I want to create a callback that's only enabled when I've set the
+# VPN tunnel to start, and as soon as it detects that the nm_applet is running,
+# then it automatically inserts the pin following roughly how the net man two
+# factor command currently works
