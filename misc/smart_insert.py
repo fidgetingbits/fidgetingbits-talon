b2fe9392d858e9071de974d3156481218f712e43
Aaron Adams <aaron.adams@nccgroup.com>
2022-05-22 09:58:41 +0800

    some old file I don't use but all is keep it for reference

---
+++ misc/smart_insert.py
@@ -0,0 +1,12 @@
+from talon import Context, Module, actions, scripting
+
+mod = Module()
+ctx = Context()
+#@ctx.action_class("main")
+#class main_actions:
+#    def insert(text: str):
+#       """override global insert action to improve speed in some scenarios"""
+#       actions.next(text)
+#       #scripting.core.main.MainActions.insert(text)
+
+
