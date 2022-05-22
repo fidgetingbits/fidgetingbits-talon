420457ab4dba8e677b1ddd0f7fd0edc82e2c5abc
Aaron Adams <aaron.adams@nccgroup.com>
2022-05-22 09:46:21 +0800

    rework a little bit of the terminal tag setting, although it's very very slow on the latest talon

--- apps/vim/vim_terminal_mode.py
+++ apps/vim/vim_terminal_mode.py
@@ -1,11 +1,16 @@
 import re
-from talon import Context, Module, actions, settings, ui
+import pprint as pp
+from timeit import default_timer as timer
+from talon import Context, Module, actions, settings, ui, registry
 
 mod = Module()
 ctx = Context()
 ctx.matches = r"""
 tag: user.vim_terminal
 """
+
+last_title = None
+last_window = None
 
 
 @ctx.action_class("edit")
@@ -37,13 +42,28 @@ def parse_vim_term_title(window):
     :returns: TODO
 
     """
-
+    global last_window
+    global last_title
+    start = timer()
     if (
         window != ui.active_window()
         or not window.title.startswith("VIM MODE:t")
         or "TERM:" not in window.title
     ):
+        end = timer()
+        #        print(start)
+        #        print(end)
+        #        print(f"parse_vim_term_title - not vim: {end - start}")
         return
+    if last_window == window and last_title == window.title:
+        end = timer()
+
+        #        print(start)
+        #        print(end)
+        #        print(f"parse_vim_term_title - duplicate: {end - start}")
+        return
+    last_window = window
+    last_title = window.title
 
     # pull a TERM: line out of something potentially like
     # VIM MODE:t RPC:/tmp/nvimlVeccr/0  TERM:gdb (term://~//161917:/usr/bin/zsh) zsh
@@ -57,31 +77,71 @@ def parse_vim_term_title(window):
         shell_command = shell_command.split(" ")[1]
     else:
         shell_command = shell_command.split(" ")[0]
+    end = timer()
+    #    print(start)
+    #    print(end)
+    #    print(f"parse_vim_term_title - pass to populate: {end - start}")
 
     populate_shell_tags(shell_command, window.title)
     populate_language_modes(shell_command)
 
 
+language_specific_commands = {
+    "repl": "python",
+}
+
+
 def populate_language_modes(shell_command):
-    """TODO: Docstring for populate_language_modes.
+    """Tries to enable language modes based off special cases of programs being
+    run, for instance running debuggers to enable gdb language, or running
+    repl to enable python language
 
-    :shell_command: TODO
-    :returns: TODO
-
+    :shell_command: The shell command being run by the terminal
     """
-    language_specific_commands = {
-        "repl": "python",
-    }
 
     for command in language_specific_commands.keys():
         if shell_command.endswith(command):
+            print("vim_terminal.py: setting context-specific language")
             actions.user.code_set_context_language(language_specific_commands[command])
             return
 
     # XXX - sometimes this throws an exception saying it's not declared, but it
     # should be a global module action from code.py
+    # Why do I clear the context language if there's no match?
     actions.user.code_clear_context_language()
     return
+
+
+# XXX - there's probably a better way to deal with this
+fuzzy_shell_tags = {
+    # Match on stuff like fzf running in floating term
+    # "term://": "user.readline",
+    "root@": "terminal",  # hacky match for docker containers
+    "python:": "user.python",
+}
+# XXX - should I pre compile these so we don't do it on every single window
+# update?
+regex_shell_tags = {
+    r"^\w*@\w*": "terminal",
+    r"^\w*@\w*:.*[$#]": "terminal",  # this is redundant with above, but ideally I would rather have something like this
+    ".*virsh start --console.*": "terminal",  # hacky match for libvirt containers
+}
+
+shell_tags = {
+    "zsh": "terminal",
+    "bash": "terminal",
+    "sh": "terminal",
+    "ssh": "terminal",
+    "sudo": "terminal",
+    "gdb": "user.gdb",
+    "pwndbg": ["user.gdb", "user.pwndbg"],
+    "gef": ["user.gdb", "user.gef"],
+    "htop": "user.htop",
+    "taskwarrior-tui": "user.taskwarrior_tui",
+    "~/.talon/bin/repl": ["user.talon_repl", "user.python"],
+    "python": "user.python",
+}
+# ...
 
 
 def populate_shell_tags(shell_command, window_title):
@@ -89,46 +149,32 @@ def populate_shell_tags(shell_command, window_title):
     :returns: TODO
 
     """
-    # XXX - this should have eventually become a list of possible tags to
-    # set?
-    shell_tags = {
-        "zsh": "terminal",
-        "bash": "terminal",
-        "sh": "terminal",
-        "ssh": "terminal",
-        "sudo": "terminal",
-        "gdb": "user.gdb",
-        "pwndbg": ["user.gdb", "user.pwndbg"],
-        "gef": ["user.gdb", "user.gef"],
-        "htop": "user.htop",
-        "taskwarrior-tui": "user.taskwarrior_tui",
-        "~/.talon/bin/repl": ["user.talon_repl", "user.python"],
-        "python": "user.python"
-    }
-    # XXX - there's probably a better way to deal with this
-    fuzzy_shell_tags = {
-        # Match on stuff like fzf running in floating term
-        # "term://": "user.readline",
-        "root@": "terminal",  # hacky match for docker containers
-        "python:": "user.python"
-    }
-    regex_shell_tags = {
-        r"^\w*@\w*": "terminal",
-        r"^\w*@\w*:.*[$#]": "terminal",  # this is redundant with above, but ideally I would rather have something like this
-        ".*virsh start --console.*": "terminal",  # hacky match for libvirt containers
-    }
+
     if shell_command in shell_tags:
+        # XXX - make a better way of marking stuff in noisy event log,
+        # ins this writes to our file/terminal
+        # actions.insert('marker start')
+        print(f"setting shell tags: {window_title}")
+        print(ctx.tags)
         if isinstance(shell_tags[shell_command], list):
+            start = timer()
             ctx.tags = shell_tags[shell_command]
-            print(ctx.tags)
         else:
+            start = timer()
             ctx.tags = [shell_tags[shell_command]]
+        end = timer()
+        print(f"populate_shell_tags: {end - start}")
+
+        # actions.insert('marker end')
+
+        # print(f"populate_shell_tags(): set {ctx.tags}")
     else:
+        print(f"trying fuzzy: {window_title}")
         found_fuzzy = False
         for tag in fuzzy_shell_tags:
-            #print(f"shell_command: {shell_command}")
+            # print(f"shell_command: {shell_command}")
             if shell_command.startswith(tag):
-                #print("found python")
+                # print("found python")
                 ctx.tags = [fuzzy_shell_tags[tag]]
                 found_fuzzy = True
                 break
@@ -142,10 +188,23 @@ def populate_shell_tags(shell_command, window_title):
             if m is not None:
                 ctx.tags = [regex_shell_tags[expression]]
                 break
+
+
 #        if not found_fuzzy:
 #            print(f"WARNING: missing tag for shell cmd: {shell_command}")
 #            print(f"WARNING: consider updating vim_terminal.py: {shell_command}")
 #
 
-ui.register("win_title", parse_vim_term_title)
-ui.register("win_focus", parse_vim_term_title)
+
+def win_title_hook(window):
+    # print(f"vim win_title_hook: {window.title}")
+    parse_vim_term_title(window)
+
+
+def win_focus_hook(window):
+    # print("vim win_focus_hook")
+    parse_vim_term_title(window)
+
+
+ui.register("win_title", win_title_hook)
+ui.register("win_focus", win_focus_hook)
