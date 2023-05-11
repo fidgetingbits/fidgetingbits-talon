9dca73a12aa39fc494fe1d08ef01de78483603bd
Aaron Adams <aaron.adams@nccgroup.com>
2023-05-11 10:36:54 +0800

    fix(spoken_forms): Fix typo in directory iteration

--- core/create_spoken_forms.py
+++ core/create_spoken_forms.py
@@ -485,7 +485,7 @@ class Actions:
                 """Return the first n items of the iterable as a list."""
                 return list(islice(iterable, n))
 
-            sources = take(DEFAULT_MAXIMUM_LIST_LENGTH, sources.diteritems())
+            sources = take(DEFAULT_MAXIMUM_LIST_LENGTH, sources.iteritems())
             app.notify(subtitle="Too many items to create spoken forms for.")
         return actions.user.create_spoken_forms_from_map(
             {source: source for source in sources},
