195041753e2f4f3a2afde63a2ad27fe539c0d5f4
Aaron Adams <aaron.adams@nccgroup.com>
2023-11-16 17:57:40 +0800

    fix: make pasting work on mac

---
+++ core/edit/paste_linux.py
@@ -0,0 +1,25 @@
+# Note: This isn't inside of edit_linux.py because of some terminal quirks, but this one does apply to everything
+from talon import Context, actions, clip, subprocess
+
+ctx = Context()
+ctx.matches = r"""
+os: linux
+"""
+
+
+@ctx.action_class("user")
+class UserActions:
+    def paste(text: str):
+        """Pastes text and preserves clipboard"""
+
+        # with clip.revert():
+        #     clip.set_text(text)
+        #     actions.edit.paste()
+        #     # sleep here so that clip.revert doesn't revert the clipboard too soon
+        #     actions.sleep("100ms")
+
+        old = subprocess.check_output(["xsel", "-o", "-b"])
+        clip.set_text(text)
+        actions.edit.paste()
+        actions.sleep("200ms")
+        clip.set_text(old.decode("utf-8"))
