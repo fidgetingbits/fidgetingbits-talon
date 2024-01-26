e5b2e30f728fe2f398e64c30c2399554d7bdfb7a
Aaron Adams <aaron.adams@nccgroup.com>
2024-01-26 18:31:52 +0800

    feat: more strace convenience

--- tags/tracing/strace.py
+++ tags/tracing/strace.py
@@ -5,6 +5,23 @@ ctx.matches = r"""
 tag: user.tracing_strace
 """
 
+ctx.lists["user.tracing_filters"] = {
+    "file": "%file",
+    "process": "%process",
+    "network": "%network",
+    "signal": "%signal",
+    "ipc": "%ipc",
+    "desc": "%desc",
+    "memory": "%memory",
+    "creds": "%creds",
+    "stat": "%stat",
+    "L stat": "%lstat",
+    "F stat": "%fstat",
+    "regex": "/",
+    "clock": "%clock",
+    "pure": "%pure",
+}
+
 
 @ctx.action_class("user")
 class Actions:
@@ -15,3 +32,11 @@ class Actions:
     def trace_program_with_log():
         """Trace command with log"""
         actions.insert("strace -f -o strace.log ")
+
+    def trace_program_with_filter():
+        """Trace command with filter"""
+        actions.user.insert_between("strace -e 'trace=", "'")
+
+    def trace_program_with_path():
+        """Trace command with path"""
+        actions.user.insert_between('strace --trace-path="', '"')
