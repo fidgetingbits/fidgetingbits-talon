778cab1f601c89d667b8d54686b6b6325f608155
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-07-11 11:39:30 +0800

    fix(zsh): fix some zsh line editor bugs

--- apps/zsh/zsh_line_editor.py
+++ apps/zsh/zsh_line_editor.py
@@ -5,8 +5,7 @@ ctx = Context()
 # We use a not tag user.readline to allow the user to pin a session temporarily to
 # use readline. For instance if from zsh you ssh into a non-zsh-based shell, etc
 ctx.matches = r"""
-tag: zsh
-not tag: user.readline
+app: zsh
 """
 
 # FIXME(zsh): This should just be a "bindkey" file, and then override various actions not just limited to readline..
@@ -15,146 +14,109 @@ not tag: user.readline
 # Need to trim down to the actual readline ones...
 # zsh line editor: https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html
 readline_commands = {
-    "set-mark-command": None,
-    "beginning-of-line": None,
-    "backward-char": None,
-    "delete-char-or-list": None,
-    "end-of-line": None,
-    "forward-char": None,
-    "send-break": None,
-    "backward-delete-char": None,
-    "expand-or-complete": None,
-    "accept-line": None,
-    "kill-line": None,
-    "clear-screen": None,
-    "accept-line": None,
-    "down-line-or-history": None,
-    "accept-line-and-down-history": None,
-    "up-line-or-history": None,
-    "push-line": None,
-    "history-incremental-search-forward": None,
-    "transpose-chars": None,
-    "kill-whole-line": None,
-    "quoted-insert": None,
-    "backward-kill-word": None,
-    "vi-match-bracket": None,
-    "edit-command-line": None,
-    "vi-find-next-char": None,
-    "vi-join": None,
-    "kill-buffer": None,
-    "infer-next-history": None,
-    "overwrite-mode": None,
-    "_read_comp": None,
-    "undo": None,
-    "vi-cmd-mode": None,
-    "exchange-point-and-mark": None,
-    "expand-word": None,
-    "what-cursor-position": None,
+    "_bash_complete-word": None,
+    "_bash_list-choices": None,
     "_complete_debug": None,
-    "_correct_filename": None,
-    "list-expand": None,
-    "_expand_alias": None,
-    "_correct_word": None,
-    "_list_expansions": None,
-    "_expand_word": None,
-    "list-expand": None,
     "_complete_help": None,
+    "_complete_tag": None,
+    "_correct_filename": None,
+    "_correct_word": None,
+    "_expand_alias": None,
+    "_expand_word": None,
+    "_history-complete-newer": None,
+    "_history-complete-older": None,
+    "_list_expansions": None,
     "_most_recent_file": None,
     "_next_tags": None,
+    "_read_comp": None,
+    "accept-and-hold": None,
+    "accept-line-and-down-history": None,
+    "accept-line": None,
+    "atuin-up-search": None,
+    "backward-char": None,
+    "backward-delete-char": None,
+    "backward-kill-word": None,
+    "backward-word": None,
+    "beginning-of-buffer-or-history": None,
+    "beginning-of-line": None,
+    "bracketed-paste": None,
+    "capitalize-word": None,
+    "clear-screen": None,
+    "copy-prev-shell-word": None,
+    "copy-prev-word": None,
+    "copy-region-as-kill": None,
+    "delete-char-or-list": None,
+    "delete-char": None,
+    "digit-argument": None,
+    "down-case-word": None,
+    "down-line-or-beginning-search": None,
+    "down-line-or-history": None,
+    "edit-command-line": None,
+    "end-of-buffer-or-history": None,
+    "end-of-line": None,
+    "exchange-point-and-mark": None,
+    "execute-last-named-cmd": None,
+    "execute-named-cmd": None,
+    "expand-history": None,
+    "expand-or-complete": None,
+    "expand-word": None,
+    "forward-char": None,
+    "forward-word": None,
+    "get-line": None,
     "history-incremental-search-backward": None,
     "history-incremental-search-forward": None,
-    "_complete_tag": None,
-    "undo": None,
-    "_bash_list-choices": None,
-    "yank": None,
-    "list-choices": None,
-    "send-break": None,
-    "backward-kill-word": None,
-    "self-insert-unmeta": None,
-    "clear-screen": None,
-    "self-insert-unmeta": None,
-    "copy-prev-word": None,
-    "expand-history": None,
-    "quote-region": None,
-    "spell-word": None,
-    "quote-line": None,
-    "_history-complete-newer": None,
-    "neg-argument": None,
-    "insert-last-word": None,
-    "_history-complete-older": None,
-    "digit-argument": None,
-    "delete-char": None,
-    "beginning-of-buffer-or-history": None,
-    "end-of-buffer-or-history": None,
-    "which-command": None,
-    "accept-and-hold": None,
-    "backward-word": None,
-    "capitalize-word": None,
-    "kill-word": None,
-    "forward-word": None,
-    "get-line": None,
-    "run-help": None,
-    "down-case-word": None,
-    "history-search-forward": None,
-    "up-line-or-beginning-search": None,
-    "atuin-up-search": None,
-    "forward-char": None,
-    "backward-char": None,
-    "end-of-line": None,
-    "beginning-of-line": None,
     "history-search-backward": None,
-    "push-line": None,
-    "spell-word": None,
-    "transpose-words": None,
-    "up-case-word": None,
-    "copy-region-as-kill": None,
-    "forward-word": None,
-    "backward-word": None,
-    "bracketed-paste": None,
-    "overwrite-mode": None,
-    "kill-word": None,
-    "delete-char": None,
-    "up-line-or-history": None,
-    "down-line-or-history": None,
-    "up-line-or-beginning-search": None,
-    "down-line-or-beginning-search": None,
-    "forward-char": None,
-    "backward-char": None,
-    "reverse-menu-complete": None,
-    "insert-last-word": None,
-    "accept-and-hold": None,
-    "backward-word": None,
-    "capitalize-word": None,
-    "kill-word": None,
-    "forward-word": None,
-    "get-line": None,
-    "run-help": None,
-    "copy-prev-shell-word": None,
     "history-search-forward": None,
-    "history-search-backward": None,
-    "push-line": None,
-    "spell-word": None,
-    "transpose-words": None,
-    "up-case-word": None,
+    "infer-next-history": None,
+    "insert-last-word": None,
+    "kill-buffer": None,
+    "kill-line": None,
     "kill-region": None,
-    "execute-named-cmd": None,
-    "yank-pop": None,
-    "execute-last-named-cmd": None,
-    "vi-goto-column": None,
-    "_bash_complete-word": None,
-    "backward-kill-word": None,
-    "undo": None,
+    "kill-whole-line": None,
+    "kill-word": None,
+    "list-choices": None,
+    "list-expand": None,
+    "neg-argument": None,
+    "overwrite-mode": None,
+    "push-line": None,
+    "quote-line": None,
+    "quote-region": None,
+    "quoted-insert": None,
+    "reverse-menu-complete": None,
+    "run-help": None,
+    "self-insert-unmeta": None,
     "self-insert": None,
-    "backward-delete-char": None,
+    "send-break": None,
+    "set-mark-command": None,
+    "spell-word": None,
+    "transpose-chars": None,
+    "transpose-words": None,
+    "undo": None,
+    "up-case-word": None,
+    "up-line-or-beginning-search": None,
+    "up-line-or-history": None,
+    "vi-cmd-mode": None,
+    "vi-find-next-char": None,
+    "vi-goto-column": None,
+    "vi-join": None,
+    "vi-match-bracket": None,
+    "what-cursor-position": None,
+    "which-command": None,
+    "yank-pop": None,
+    "yank": None,
 }
+
 
 def _trigger_keys(entry):
     if entry in readline_commands:
+        print(f"{entry}: {readline_commands[entry]}")
         for key in readline_commands[entry]:
+            print(f"key: {key}")
             actions.key(key)
     else:
         print(f"Unbound readline command: {entry}")
         actions.next()
+
 
 # This almost definitley needs some Mac-specific switching
 zsh_key_to_talon_map = {
@@ -175,7 +137,7 @@ for line in output.split("\n"):
     keys = []
     bindkeys = list(bindkey)[1:-1]
     i = 0
-    print(f"bindkeys: {bindkeys}")
+    # print(f"bindkeys: {bindkeys}")
     # "^[ " expand-history breaks this
     if len(bindkeys) == 1 and bindkeys[0] == "^":
         continue
@@ -187,12 +149,13 @@ for line in output.split("\n"):
         else:
             keys.append(char)
         i += 1
-    print(f"keys: {keys}")
+    # print(f"keys: {keys}")
     if command in readline_commands:
         readline_commands[command] = keys
 
 # "\M-^@"-"\M-^?" self-insert breaks shit as well
-print(readline_commands)
+# print(readline_commands)
+
 
 @ctx.action_class("edit")
 class EditActions:
@@ -229,7 +192,13 @@ class EditActions:
     # Modifying Text - https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#Modifying-Text
     ##
     def delete_line():
-        _trigger_keys("kill-whole-line")
+        # WARNING: This should be kill-whole-line, but the default is ctrl-shift-u, which depending on your window
+        # manager may be overridden by default. For instance, on GNOME, this is the unicode hotkey:
+        # ❯ gsettings list-recursively | grep 'unicode-hotkey'
+        # org.freedesktop.ibus.panel.emoji unicode-hotkey ['<Control><Shift>u']
+        # So we just combo it
+        actions.edit.line_start()
+        actions.user.delete_line_end()
 
     ##
     # Miscellaneous - https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#Miscellaneous
@@ -239,6 +208,7 @@ class EditActions:
 
     def redo():
         _trigger_keys("redo")
+
 
 @ctx.action_class("user")
 class UserActions:
@@ -262,16 +232,18 @@ class UserActions:
     ##
 
     def word_left_long():
-        _trigger_keys("vi-backward-blank-word"):
+        _trigger_keys("vi-backward-blank-word")
 
     def word_right_long():
-        _trigger_keys("vi-forward-blank-word"):
+        _trigger_keys("vi-forward-blank-word")
 
-    def jump_cursor_to_next_char():
+    def jump_cursor_to_next_char(key: str):
         _trigger_keys("vi-find-next-char")
+        actions.key(key)
 
-    def jump_cursor_to_prev_char():
+    def jump_cursor_to_prev_char(key: str):
         _trigger_keys("vi-find-prev-char")
+        actions.key(key)
 
     ##
     # Miscellaneous - https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#Miscellaneous
