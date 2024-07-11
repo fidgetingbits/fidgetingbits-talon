2b459ec6ff665c1afe743ea3923855e97eb138fc
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-07-11 15:13:33 +0800

    fix(zsh): fix more line editor functionality

--- apps/zsh/zsh_line_editor.py
+++ apps/zsh/zsh_line_editor.py
@@ -1,19 +1,20 @@
 import subprocess
-from talon import Context, actions
+import pprint
+from talon import Module, Context, actions
 
+mod = Module()
 ctx = Context()
 # We use a not tag user.readline to allow the user to pin a session temporarily to
 # use readline. For instance if from zsh you ssh into a non-zsh-based shell, etc
 ctx.matches = r"""
 app: zsh
+and not tag: user.readline
 """
-
-# FIXME(zsh): This should just be a "bindkey" file, and then override various actions not just limited to readline..
 
 # This is everything from bindkey that is set, but missing everything that isn't set
 # Need to trim down to the actual readline ones...
 # zsh line editor: https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html
-readline_commands = {
+zle_keymap = {
     "_bash_complete-word": None,
     "_bash_list-choices": None,
     "_complete_debug": None,
@@ -36,6 +37,7 @@ readline_commands = {
     "backward-char": None,
     "backward-delete-char": None,
     "backward-kill-word": None,
+    "backward-kill-line": None,
     "backward-word": None,
     "beginning-of-buffer-or-history": None,
     "beginning-of-line": None,
@@ -108,13 +110,13 @@ readline_commands = {
 
 
 def _trigger_keys(entry):
-    if entry in readline_commands:
-        print(f"{entry}: {readline_commands[entry]}")
-        for key in readline_commands[entry]:
+    if entry in zle_keymap:
+        print(f"{entry}: {zle_keymap[entry]}")
+        for key in zle_keymap[entry]:
             print(f"key: {key}")
             actions.key(key)
     else:
-        print(f"Unbound readline command: {entry}")
+        print(f"Unbound zle command: {entry}")
         actions.next()
 
 
@@ -123,38 +125,66 @@ zsh_key_to_talon_map = {
     "^": "ctrl",
 }
 
-output = subprocess.check_output(("zsh", "-c", "bindkey")).decode("utf-8")
+zle_command_ignore_list = [
+    '"ls^J"',
+    '" magic-space',
+]
 
-for line in output.split("\n"):
-    if not line.strip():
-        continue
 
-    bindkey, command = line.split(None, 1)
-    command = command.strip()
+def _read_zle_keymap():
+    """Reads the output from `bindkey` and populates a map the keybindings.
 
-    # bindkey is something like this: "^[[A" up-line-or-beginning-search
-    # We want to pass each to key() as "ctrl-[", "[", "A"
-    keys = []
-    bindkeys = list(bindkey)[1:-1]
-    i = 0
-    # print(f"bindkeys: {bindkeys}")
-    # "^[ " expand-history breaks this
-    if len(bindkeys) == 1 and bindkeys[0] == "^":
-        continue
-    while i < len(bindkeys):
-        char = bindkeys[i]
-        if char in zsh_key_to_talon_map:
-            keys.append(f"{zsh_key_to_talon_map[char]}-{bindkeys[i+1]}")
+    Note we use an interactive zsh shell to get the keybindings."""
+
+    global zle_keymap
+    output = subprocess.check_output(("zsh", "-ic", "bindkey")).decode("utf-8")
+    # print(output)
+    for line in output.split("\n"):
+        if not line.strip():
+            continue
+
+        bindkey, command = line.split(None, 1)
+        command = command.strip()
+
+        # bindkey is something like this: "^[[A" up-line-or-beginning-search
+        # We want to pass each to key() as "ctrl-[", "[", "A"
+        keys = []
+        bindkeys = list(bindkey)[1:-1]
+        i = 0
+        # print(f"bindkeys: {bindkeys}")
+        # "^[ " expand-history breaks this
+        if len(bindkeys) == 1 and bindkeys[0] == "^":
+            continue
+        while i < len(bindkeys):
+            char = bindkeys[i]
+            if char in zsh_key_to_talon_map:
+                keys.append(f"{zsh_key_to_talon_map[char]}-{bindkeys[i+1].lower()}")
+                i += 1
+            else:
+                keys.append(char)
             i += 1
-        else:
-            keys.append(char)
-        i += 1
-    # print(f"keys: {keys}")
-    if command in readline_commands:
-        readline_commands[command] = keys
+        # print(f"keys: {keys}")
 
-# "\M-^@"-"\M-^?" self-insert breaks shit as well
-# print(readline_commands)
+        if command in zle_keymap:
+            zle_keymap[command] = keys
+        else:
+            if command not in zle_command_ignore_list:
+                print(
+                    f"Unsupported command: ' {command} ', update zle_keymap in zsh_line_editor.py"
+                )
+
+    # "\M-^@"-"\M-^?" self-insert breaks this as well
+    # print(f"zle_keymap: {pprint.pformat(zle_keymap)}")
+
+
+_read_zle_keymap()
+
+
+@mod.action_class
+class Actions:
+    def zle_update_keymap():
+        """Updates the zle keymap"""
+        _read_zle_keymap()
 
 
 @ctx.action_class("edit")
@@ -174,12 +204,6 @@ class EditActions:
     def right():
         _trigger_keys("forward-char")
 
-    def word_left():
-        _trigger_keys("backward-word")
-
-    def word_right():
-        _trigger_keys("forward-word")
-
     def line_start():
         # This will skip to earlier lines if already at the start
         _trigger_keys("beginning-of-line")
@@ -192,13 +216,7 @@ class EditActions:
     # Modifying Text - https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#Modifying-Text
     ##
     def delete_line():
-        # WARNING: This should be kill-whole-line, but the default is ctrl-shift-u, which depending on your window
-        # manager may be overridden by default. For instance, on GNOME, this is the unicode hotkey:
-        # ❯ gsettings list-recursively | grep 'unicode-hotkey'
-        # org.freedesktop.ibus.panel.emoji unicode-hotkey ['<Control><Shift>u']
-        # So we just combo it
-        actions.edit.line_start()
-        actions.user.delete_line_end()
+        _trigger_keys("kill-whole-line")
 
     ##
     # Miscellaneous - https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#Miscellaneous
@@ -221,20 +239,32 @@ class UserActions:
     def delete_line_end():
         _trigger_keys("kill-line")
 
-    def delete_word_right():
-        _trigger_keys("delete-word")
+    def delete_word_short_right():
+        _trigger_keys("kill-word")
 
-    def delete_word_left():
-        _trigger_keys("backward-delete-word")
+    def delete_word_short_left():
+        _trigger_keys("backward-kill-word")
+
+    def delete_word_long_right():
+        _trigger_keys("kill-word")
+
+    def delete_word_long_left():
+        _trigger_keys("backward-kill-word")
 
     ##
     # Movement - https://zsh.sourceforge.io/Doc/Release/Zsh-Line-Editor.html#Movement
     ##
 
-    def word_left_long():
+    def word_short_left():
+        _trigger_keys("backward-word")
+
+    def word_short_right():
+        _trigger_keys("forward-word")
+
+    def word_long_left():
         _trigger_keys("vi-backward-blank-word")
 
-    def word_right_long():
+    def word_long_right():
         _trigger_keys("vi-forward-blank-word")
 
     def jump_cursor_to_next_char(key: str):
