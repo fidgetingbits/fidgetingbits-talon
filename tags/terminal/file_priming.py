cfb6d8bbae1231620d9121b332d1b8da3b70eb41
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-07-24 15:00:32 +0800

    feat(templates): support quick dev flake.nix templating

---
+++ tags/terminal/file_priming.py
@@ -0,0 +1,50 @@
+from talon import Context, Module
+
+mod = Module()
+mod.list(
+    "nix_way_template_languages",
+    "Supported languages for the-nix-way flake dev-templates",
+)
+
+ctx = Context()
+ctx.matches = r"""
+os: linux
+app: terminal
+"""
+
+# https://github.com/the-nix-way/dev-templates
+ctx.lists["user.nix_way_template_languages"] = {
+    "bun": "bun",
+    "c": "c-cpp",
+    "clojure": "clojure",
+    "c sharp": "csharp",
+    "cue": "cue",
+    "dhall": "dhall",
+    "elixir": "elixir",
+    "elm": "elm",
+    "gleam": "gleam",
+    "go": "go",
+    "hashicorp": "hashi",
+    "haxe": "haxe",
+    "java": "java",
+    "kotlin": "kotlin",
+    "latex": "latex",
+    "nickel": "nickel",
+    "nim": "nim",
+    "nix": "nix",
+    "node": "node",
+    "ocaml": "ocaml",
+    "open policy agent": "opa",
+    "php": "php",
+    "proto buf": "protobuf",
+    "pulumi": "pulumi",
+    "purescript": "purescript",
+    "r": "r",
+    "ruby": "ruby",
+    "rust": "rust",
+    "scala": "scala",
+    "shell": "shell",
+    "swift": "swift",
+    "vlang": "vlang",
+    "zig": "zig",
+}
