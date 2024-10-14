3adedb9407ae02031a0e38fbc63e83eb06c2828c
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-10-14 15:00:50 +0800

    refactor(ida): move app declaration

--- core/application_matches.py
+++ core/application_matches.py
@@ -140,15 +140,6 @@ os: linux
 and app.name: "draw.io"
 """
 
-apps.ida = """
-os: linux
-and app.name: IDA
-"""
-
-apps.hexrays_vault = """
-win.title:/IDA Teams/
-"""
-
 apps.microsoft_teams = """
 os: linux
 and app.name: Teams
