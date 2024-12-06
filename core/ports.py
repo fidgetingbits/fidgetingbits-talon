3a5a15a501b9f980b75d32bd504d9ca518716fa4
fidgetingbits <fidgetingbits@users.noreply.github.com>
2024-12-06 15:31:19 +0800

    feat(ports): add a few server ports

--- core/ports.py
+++ core/ports.py
@@ -4,7 +4,7 @@ mod = Module()
 mod.list("ports", desc="Common ports")
 
 ctx = Context()
-# TODO: We could probably just auto populate this with some file
+# TODO: We could probably just auto populate a bunch of this with some file
 ctx.lists["user.ports"] = {
     "F T P": "21",
     "S S H": "22",
@@ -13,6 +13,9 @@ ctx.lists["user.ports"] = {
     "remote desktop": "3389",
     "redis": "6379",
     "sync thing": "8384",
+    "hex ray's teams server": "65433",
+    "hex ray's lumina server": "65432",
+    "hex ray's license server": "65434",
 }
 
 
