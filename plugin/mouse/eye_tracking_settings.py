19a11b866bf177e0bc2dcc921c3f929fee846a7b
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-01-15 11:49:11 +0800

    refactor(mouse): get rid of most plugin errors first stuff I don't use

--- plugin/mouse/eye_tracking_settings.py
+++ plugin/mouse/eye_tracking_settings.py
@@ -1,13 +1,13 @@
-from talon import app
-from talon.types import Point2d
-from talon_plugins import eye_zoom_mouse
+# from talon import app
+# from talon.types import Point2d
+# from talon_plugins import eye_zoom_mouse
 
-if app.platform == "mac":
-    eye_zoom_mouse.config.screen_area = Point2d(100, 75)
-    eye_zoom_mouse.config.img_scale = 6
-elif app.platform == "win":
-    eye_zoom_mouse.config.screen_area = Point2d(200, 150)
-    eye_zoom_mouse.config.img_scale = 4.5
-elif app.platform == "linux":
-    eye_zoom_mouse.config.screen_area = Point2d(200, 150)
-    eye_zoom_mouse.config.img_scale = 6
+# if app.platform == "mac":
+#     eye_zoom_mouse.config.screen_area = Point2d(100, 75)
+#     eye_zoom_mouse.config.img_scale = 6
+# elif app.platform == "win":
+#     eye_zoom_mouse.config.screen_area = Point2d(200, 150)
+#     eye_zoom_mouse.config.img_scale = 4.5
+# elif app.platform == "linux":
+#     eye_zoom_mouse.config.screen_area = Point2d(200, 150)
+#     eye_zoom_mouse.config.img_scale = 6
