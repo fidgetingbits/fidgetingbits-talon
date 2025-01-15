63cf892a18150b71726e6f8d2fcf0bf7a37c2819
fidgetingbits <fidgetingbits@users.noreply.github.com>
2025-01-15 11:36:03 +0800

    feat: add more network ports

--- core/ports.py
+++ core/ports.py
@@ -2,12 +2,15 @@ from talon import Context, Module
 
 mod = Module()
 mod.list("ports", desc="Common ports")
+mod.list("network_protocols", desc="Common Net Work ports")
 
 ctx = Context()
 # TODO: We could probably just auto populate a bunch of this with some file
 ctx.lists["user.ports"] = {
     "F T P": "21",
     "S S H": "22",
+    "telnet": "23",
+    "T F T P": "69",
     "web": "80",
     "T L S": "443",
     "remote desktop": "3389",
@@ -16,6 +19,12 @@ ctx.lists["user.ports"] = {
     "hex ray's teams server": "65433",
     "hex ray's lumina server": "65432",
     "hex ray's license server": "65434",
+}
+
+ctx.lists["user.network_protocols"] = {
+    "T C P": "tcp",
+    "U D P": "udp",
+    "I C M P": "icmp",
 }
 
 
